42e6944d6fe5c9a9ff1c6daeb2e63b1863daa6af
Nur Fajri Qas <nfq.fajri28@gmail.com>
2022-11-08 00:54:31 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
-// import Router from './Routes' ;
 import './App.css'
-// import { Dashboard } from './pages/Dashboard';
-import { RegisterPage } from './pages/Register';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import RegisterPage from './pages/Register/RegisterPage';
+import Dashboard from './pages/Dashboard/Dashboard';
+
+
 
 function App() {
   return (
-    <div className='App'>
-      <RegisterPage/>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/RegisterPage' element={<RegisterPage type='RegisterPage'/>}/>
+          <Route path='/Dashboard' element={<Dashboard type='Dashboard'/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
--- client/src/Routes/index.js
+++ client/src/Routes/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import RegisterPage from './../pages/Register/RegisterPage'
-import Dashboard from './../pages/Dashboard/Dashboard'
+import RegisterPage from '../pages/Register/RegisterPage'
+import Dashboard from '../pages/Dashboard/Dashboard'
 
 export default function Router() {
     return (
